c437f2e8286d5e1ebba52a9773d280d8959c8047
DSRCODE <daljeetkalyanwat62@gmail.com>
2025-04-07 22:28:15 +0530

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          border: "1px  dashed",
+        }}
+      >
+        <h1 style={{ fontWeight: "600", textTransform: "uppercase" }}>
+          LPG Landing Page
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
